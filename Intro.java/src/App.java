6d21c7684ed5f357eb495fddbfeb511649b9f83d
will <wijelle@gmail.com>
2023-10-09 22:40:58 -0400

    Update App.java

--- Intro.java/src/App.java
+++ Intro.java/src/App.java
@@ -3,6 +3,7 @@
 //My name is Jane Doe and my favorite color is green and I love Ryan Reynolds.
 //Hi my name is Ayan and my fun fact is that I love cats
 //Hi my name is Cici and a fun fact about me is that I can reach up to a wpm of 151.
+// Hello my name is Will and a fun fact about me is that I love math. Math is so fun and interesting and I love to study math because it satisfies me to an unbeknown and unfathomable extent. Math is love, math is life. QED.
 //Hi My name is Arushi and a fun fact about me is that I hate the smell of cheese. 
 // Hi My name is Maria and my favorite artists are Travis Scott, Drake, and Kali Uchis. 
 //Hi my name is Ayan and my fun fact is that I love cats
