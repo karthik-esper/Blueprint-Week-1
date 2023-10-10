ce4ac799c11d2960259e8de5c1ebc8f1cfd7fde4
karthik-esper <kgangireddy04@gmail.com>
2023-10-09 20:36:04 -0400

    Added my comment
    Added comment about Karthik

--- .history/Intro.java/src/App_20231009203045.java
+++ .history/Intro.java/src/App_20231009203045.java
@@ -1,4 +1,4 @@
 //Hi! My name is Arushi! My least favorite app is DUO Mobile. A fun fact about me is that I used to cry everytime I would go to the optometrist as a kid. Something that I am listening to right now is Drake's new album: For all the dogs. 
 //Hi my name is John Carlo! My favorite invention is the air conditioning unit.
 //My name is Jane Doe and my favorite color is green and I love Ryan Reynolds. 
-//My name is Karthik and a fun fact about me is that I've lived in 3 different countries and 4 different states throughout my life. 
+/
